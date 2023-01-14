68d30de8c8b6e87d7cf74f9c5990cfcf3b9e1156
lby132 <quddbscjswow@naver.com>
2023-01-15 01:06:07 +0900

    board

--- src/main/java/com/godcoder/myrest/controller/BoardController.java
+++ src/main/java/com/godcoder/myrest/controller/BoardController.java
@@ -6,15 +6,15 @@ import com.godcoder.myrest.validator.BoardValidator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -31,11 +31,13 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model, @PageableDefault(size = 2) Pageable pageable) {
-        final Page<Board> boards = boardRepository.findAll(pageable);
+    public String list(Model model, @PageableDefault(size = 2) Pageable pageable,
+                       @RequestParam(required = false, defaultValue = "") String searchText) {
+        //final Page<Board> boards = boardRepository.findAll(pageable);
+        final Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+
         final int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
         final int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
-
         model.addAttribute("startPage", startPage);
         model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
