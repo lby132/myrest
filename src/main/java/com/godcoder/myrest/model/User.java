55852982cb6970aae4ea9e79d74c31b98d6f6c83
pro <quddbscjswow@naver.com>
2023-01-22 12:53:06 +0900

    orpanremoval

--- src/main/java/com/godcoder/myrest/model/User.java
+++ src/main/java/com/godcoder/myrest/model/User.java
@@ -28,6 +28,6 @@ public class User {
     private List<Role> roles = new ArrayList<>();
 
     @JsonIgnore
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Board> boards = new ArrayList<>();
 }
